3b913f7fb368844051db11a97e5026b65750772c
Nevets <12600557+Nevets10@users.noreply.github.com>
2024-04-04 17:41:48 -0700

    Add functionality for navigating to pages

--- server.js
+++ server.js
@@ -5,8 +5,25 @@ const events = require("events");
 
 http
 	.createServer((req, res) => {
-		res.writeHead(200, { "Content-Type": "text/html" });
-		res.write("Served.");
-		res.end();
+		const q = url.parse(req.url, true);
+		const fileName = q.pathname !== "/" ? `.${q.pathname}.html` : "./index.html";
+
+		fs.readFile(fileName, (err, data) => {
+			if (err) {
+				return notFound(req, res);
+			} else {
+				res.writeHead(200, { "Content-Type": "text/html" });
+				res.write(data);
+				return res.end();
+			}
+		});
 	})
 	.listen(8080);
+
+function notFound(req, res) {
+	fs.readFile("./404.html", (err, data) => {
+		res.writeHead(404, { "Content-Type": "text/html" });
+		res.write(data);
+		return res.end();
+	});
+}
